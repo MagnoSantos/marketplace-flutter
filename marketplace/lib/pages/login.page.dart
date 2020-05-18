4ea75d75721dfda6973da5b22c4819742efb45a8
Magno Juliano Gonçalves Santos <santos.magno@mrv.com.br>
2020-05-18 00:26:43 -0300

    Alteração e inclusão da tela de sign up

--- marketplace/lib/pages/login.page.dart
+++ marketplace/lib/pages/login.page.dart
@@ -1,188 +1,203 @@
 import 'package:flutter/material.dart';
+import 'package:marketplace/pages/signup.page.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          color: Color(0xFFF5F5F5F5),
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 380,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    new BoxShadow(
-                      color: Colors.black12,
-                      //Quantidade versus projeção da sombra
-                      offset: new Offset(3, 4.0),
-                      blurRadius: 5,
-                      spreadRadius: 1,
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: ListView(
-                    children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            children: <Widget>[
-                              Text(
-                                "Bem vindo,",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w500,
+        body: SingleChildScrollView(
+          child: Container(
+            color: Color(0xFFF5F5F5F5),
+            padding: EdgeInsets.only(
+              top: 60,
+              right: 20,
+              left: 20,
+              bottom: 20,
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 380,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.black12,
+                        //Quantidade versus projeção da sombra
+                        offset: new Offset(3, 4.0),
+                        blurRadius: 5,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: ListView(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  "Bem vindo,",
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
-                              ),
-                              Text("Entre para continuar"),
-                            ],
-                          ),
-                          FlatButton(
-                            onPressed: () {},
-                            child: Text("Inscrever"),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        autofocus: true,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
+                                Text("Entre para continuar"),
+                              ],
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => InscreverPage(),
+                                  ),
+                                );
+                              },
+                              child: Text("Inscrever"),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          autofocus: true,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextFormField(
-                        autofocus: true,
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: "Senha",
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 16,
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          autofocus: true,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Senha",
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                      ),
-                      FlatButton(
-                        onPressed: () {},
-                        child: Text("Esqueceu sua senha?"),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5),
-                          ),
-                        ),
-                        child: FlatButton(
+                        FlatButton(
                           onPressed: () {},
-                          child: Text(
-                            "Entrar",
-                            style: TextStyle(color: Colors.white),
+                          child: Text("Esqueceu sua senha?"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                          child: FlatButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Entrar",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  child: Text(
+                    "- OU -",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2.0,
+                      color: Colors.black12,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: FlatButton(
+                    onPressed: () {},
+                    //Para adicionar imagem
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          height: 24,
+                          child: Image.asset(
+                            "assets/facebook.png",
+                            width: 24,
                           ),
                         ),
-                      )
-                    ],
+                        Text("Entrar com Facebook"),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                child: Text(
-                  "- OU -",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: double.infinity,
-                height: 60,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2.0,
-                    color: Colors.black12,
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2.0,
+                      color: Colors.black12,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
-                  ),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  //Para adicionar imagem
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        width: 100,
-                        height: 24,
-                        child: Image.asset(
-                          "assets/facebook.png",
-                          width: 24,
+                  child: FlatButton(
+                    onPressed: () {},
+                    //Para adicionar imagem
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 100,
+                          height: 24,
+                          child: Image.asset(
+                            "assets/google.png",
+                            width: 24,
+                          ),
                         ),
-                      ),
-                      Text("Entrar com Facebook"),
-                    ],
+                        Text("Entrar com Google"),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: double.infinity,
-                height: 60,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 2.0,
-                    color: Colors.black12,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5),
-                  ),
-                ),
-                child: FlatButton(
-                  onPressed: () {},
-                  //Para adicionar imagem
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        width: 100,
-                        height: 24,
-                        child: Image.asset(
-                          "assets/google.png",
-                          width: 24,
-                        ),
-                      ),
-                      Text("Entrar com Google"),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
